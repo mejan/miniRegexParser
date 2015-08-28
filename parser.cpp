67e0224664cdcafaafda4a13635cb645f3c755c1
mejan <najemes0@gmail.com>
2015-08-28 11:59:08 +0200

    total remake of parser class to retry

---
+++ parser.cpp
@@ -0,0 +1,25 @@
+#include "parser.h"
+
+parser::parser(){}
+
+parser::~parser(){}
+
+void parser::expresion(){
+
+}
+
+bool parser::orOperation(){
+	return 0;
+}
+
+bool parser::parOperation(){
+	return 0;
+}
+
+bool parser::conOperation(){
+	return 0;
+}
+
+bool parser::repOperation(){
+	return 0;
+}
---
+++ parser.h
@@ -0,0 +1,34 @@
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include "regex.h"
+
+class parser{
+	public:
+		/*
+		*Constructors and destructors.
+		*/
+		// Default constuctor set private datamebers to basicly zero.
+		parser();
+		// Dedtructor takes care of memory resturn (maybe not (not sure yet)).
+		~parser();
+
+		/*
+		*Public member functions in parser
+		*/
+	private:
+		/*
+		*Private member functions in parser
+		*/
+		// Expresion bas function for the parsing.
+		void expresion();
+		// How we work with the or sign +.
+		bool orOperation();
+		// How we work with the ()'s.
+		bool parOperation();
+		// Concat/Union's function.
+		bool conOperation();
+		// repeat function.
+		bool repOperation();
+};
+#endif /*_PARSER_H_*/
